65e01d46680a77257d19e361d350c955546da7e2
oleks <s27565@pjwstk.edu.pl>
2024-05-14 19:58:57 +0200

    1

--- src/ListaZyczen.java
+++ src/ListaZyczen.java
@@ -5,7 +5,6 @@ public class ListaZyczen  {
 
      static HashMap<String,ArrayList<Pakiet>> mapListZyczen=new HashMap<>();
     private String klientID;
-    private Pakiet pakiet;
 
     public ListaZyczen(String klientID) {
        mapListZyczen.put(klientID,new ArrayList<>());
@@ -14,43 +13,42 @@ public class ListaZyczen  {
 
 
     public void add(Pakiet pakiet){
-        this.pakiet=pakiet;
-        findCena();
-      mapListZyczen.get(klientID).add(this.pakiet);
+        findCena(pakiet);
+      mapListZyczen.get(klientID).add(pakiet);
     }
     public ArrayList<Pakiet> getListaZyczen(){
         return ListaZyczen.mapListZyczen.get(klientID);
     }
-    private void findCena() {
+    private void findCena(Pakiet pakiet) {
         Cennik cennik=Cennik.pobierzCennik();
-        PaketInfo paketInfo=cennik.findByName(this.pakiet.getNazwa());
+        PaketInfo paketInfo=cennik.findByName(pakiet.getNazwa());
         if(paketInfo!=null){
         switch (paketInfo.getTyp()){
             case KROTKI -> {
-                if(this.pakiet.getIlosc()==1) this.pakiet.setCena(paketInfo.getCenaJeden());
-                else if(this.pakiet.getIlosc()<= paketInfo.getLimit()) this.pakiet.setCena(paketInfo.getCenaUnderLimit());
-                else if(this.pakiet.getIlosc() > paketInfo.getLimit())this.pakiet.setCena(paketInfo.getCenaOverLimit());
+                if(pakiet.getIlosc()==1) pakiet.setCena(paketInfo.getCenaJeden());
+                else if(pakiet.getIlosc()<= paketInfo.getLimit()) pakiet.setCena(paketInfo.getCenaUnderLimit());
+                else if(pakiet.getIlosc() > paketInfo.getLimit())pakiet.setCena(paketInfo.getCenaOverLimit());
 
             }
             case SREDNI -> {
-                if(this.pakiet.getIlosc()<= paketInfo.getLimit()) this.pakiet.setCena(paketInfo.getCenaUnderLimit());
-                else this.pakiet.setCena(paketInfo.getCenaOverLimit());
+                if(pakiet.getIlosc()<= paketInfo.getLimit()) pakiet.setCena(paketInfo.getCenaUnderLimit());
+                else pakiet.setCena(paketInfo.getCenaOverLimit());
 
             }
             case DLUGI -> { for(Klient klient: Klient.klients){
                 if(klient.getIdentyficator().equals(klientID)) {
-                    if(klient.getAbonament().equals(Abonament.TAK)) this.pakiet.setCena(paketInfo.getCenaAbonament());
-                    else this.pakiet.setCena(paketInfo.getCenaBezAbonament());
+                    if(klient.getAbonament().equals(Abonament.TAK)) pakiet.setCena(paketInfo.getCenaAbonament());
+                    else pakiet.setCena(paketInfo.getCenaBezAbonament());
                     }
 
                 }
             }
             case DARMO -> {
-                this.pakiet.setCena(0);
+                pakiet.setCena(0);
 
             }
         }
-        } else this.pakiet.setCena(-1);
+        } else pakiet.setCena(-1);
 
 
     }
